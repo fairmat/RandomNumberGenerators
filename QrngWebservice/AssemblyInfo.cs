f70e2389d39611b4122c4ec7f1b1db5bf65f74ba
Giulio Fornasaro <giulio.fornasaro@fairmat.com>
2024-04-19 17:19:06 +0200

    [RD-FFK-NET8] Readded missing AssemblyInfo file fairmat/dev/fairmatserver#3493

---
+++ QrngWebservice/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using Mono.Addins;
+
+// Plugin information
+[assembly: Addin("QRNG UniBerlin", "1.0.2", Category = "Random numbers generator")]
+[assembly: AddinDependency("Fairmat", "1.0")]
+[assembly: AddinDependency("Random Sources Support", "1.0.0")]
+[assembly: AddinAuthor("Fairmat SRL")]
